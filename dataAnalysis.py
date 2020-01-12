6048e4e2c943bd342cbdf76f1dec2ef28d85f422
Ryan Zhan <ryanzhan911@gmail.com>
2020-01-12 13:57:58 -0800

    added looping function for PS of multiple data sets

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -42,18 +42,52 @@ data_file.close()
 print("Total amount of data sets: ",dataSet+1)
 #Getting the power spectra
 
-#The following commented code is wrong. I'm not sure why buy it returns a power spectra with negative values which is impossible
-""" plt.figure(1)
-sp = (fft.rfft(xacel[2]))**2
-freqrange = np.linspace(0, 50/2, len(sp))
-plt.plot(freqrange,sp)
- """
-plt.figure(1)
-#Use numpy's libraries to calculate the FFT and then squre it to get the pwoer spectra.
-psx = np.abs((fft.fft(xacel[2])))**2
-freq = fft.fftfreq(len(xacel[2]),d=.02)
+#for each set of data take the fft and add it to the average
+PSAveX = np.empty(1024)
+count = 0 #Number of valid power spectrum calculations that we can average. 
+for xset in xacel:
+    #for every set of data, calculate the power spectra
+    psx = np.abs((fft.fft(xset)))**2
+    # we only want sets of data that we can average so check their lengths
+    if len(psx) == 1024:
+        #if this set has the right length, add it to the overal power spectra list to be averaged
+        i = 0
+        for val in psx:
+            PSAveX[i] += val
+            i += 1
+        #increment count to keep track of how many valid power spectra there are
+        count += 1
+        #calc the frequency bins using only valid datasets
+        #the current placement of this isnt optimal but what are you going to do?
+    
+        freq = fft.fftfreq(len(xset),d=.02)
+
+"""     if len(psx) == 1024:
+        i = 0
+        for val in psx:
+            PSAveX[i] += val
+            i += 1
+        count += 1 """
+
+
+#PSAveX = [x/count for x in PSAveX]
+plt.figure(0)
+psx = np.abs((fft.fft(xacel[200])))**2
+""" freq = fft.fftfreq(len(xacel[2]),d=.02) """
+plt.plot(freq,PSAveX)
+
+plt.figure(5)
+psx = np.abs((fft.fft(xacel[200])))**2
+freq = fft.fftfreq(len(xacel[200]),d=.02)
 plt.plot(freq,psx)
 
+""" plt.figure(1)
+#Use numpy's libraries to calculate the FFT and then squre it to get the pwoer spectra.
+#psx = np.abs((fft.fft(xacel[316])))**2
+freq = fft.fftfreq(len(xacel[2]),d=.02)
+plt.plot(freq,PSAveX)
+print(len(psx))
+ """
 #create power spectrum for y axis.
 plt.figure(2)
 psy = np.abs((fft.fft(yacel[2])))**2
