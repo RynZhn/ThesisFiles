979b8d8f05144dc544f0c0c3a3806fc355dbad85
RynZhn <ryanzhan911@gmail.com>
2020-01-13 12:19:51 -0800

    Moved functionality for power spectrum to seperate file

---
+++ PowerSpectrumLib.py
@@ -0,0 +1,10 @@
+def CalcPowerSpec (dataArray,dataRate):
+    """ This function removes a fixed bias from the data
+    and calculates the power spectrum and frequency range of the dataset.
+    The output is two arrays. Input is data array and time between datapoints
+    """
+    mean = sum(dataArray)/len(dataArray)
+    correctedArray = [x-mean for x in dataArray]
+    ps = np.abs((fft.fft(correctedArray)))**2
+    freq = fft.fftfreq(len(correctedArray),d=dataRate)
+    return [ps,freq]
--- dataAnalysis.py
+++ dataAnalysis.py
@@ -3,17 +3,7 @@ import numpy.fft as fft
 import numpy as np
 import os
 
-def CalcPowerSpec (dataArray,dataRate):
-    """ This function removes a fixed bias from the data
-    and calculates the power spectrum and frequency range of the dataset.
-    The output is two arrays. Input is data array and time between datapoints
-    """
-    mean = sum(dataArray)/len(dataArray)
-    correctedArray = [x-mean for x in dataArray]
-    ps = np.abs((fft.fft(correctedArray)))**2
-    freq = fft.fftfreq(len(correctedArray),d=dataRate)
-    return [ps,freq]
-
+from PowerSpectrumLib import CalcPowerSpec
 
 # create a list for the different accelleration data
 xacel, yacel, zacel = [[]],[[]],[[]]
