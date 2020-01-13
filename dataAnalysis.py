d4c9998fc9f456bce88cecfd22d03fe9af2b587a
RynZhn <ryanzhan911@gmail.com>
2020-01-13 13:34:35 -0800

    added more verbose feedback for errors

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -41,7 +41,7 @@ for line in data_file:
 
 data_file.close()
 
-print("Total amount of data sets: ",dataSet+1)
+print("Total amount of data sets: ",dataSet)
 
 startIndex,endIndex = input("Enter which data sets you would like to get the PS [ x , y ] (sets x to y) ").split(",")
 
@@ -51,36 +51,19 @@ endIndex = int(endIndex)
 #try and average the above power spectrums
 validSets = 0 #keeps track of the number of valid data sets so we know how to average
 PSXAve = [0]*1024
+currentIndex = startIndex
 for dataSet in xacel[startIndex:endIndex]:
     [ps,freq] = CalcPowerSpec(dataSet,.02)                             #get the power spectrum
     if len(ps) == 1024:                                                 #check to see if power spectrum is valid length
         validSets += 1                                                      #if it is, incrememnt the count
         PSXAve = [x + y for x,y in zip(PSXAve,ps)]                      #and add the new ps to the total sum
-
+    else:
+        print("Bad length for index: ",currentIndex)
+    currentIndex += 1
 PSXAve = [x/validSets for x in PSXAve]                                      #divide each element by the total amount to get the average
 
 
 plt.figure(5)
-plt.plot(freq,PSXAve)
-
-
-startIndex,endIndex = input("Enter which data sets you would like to get the PS [ x , y ] (sets x to y) ").split(",")
-
-startIndex = int(startIndex)
-endIndex = int(endIndex)
-
-#try and average the above power spectrums
-validSets = 0 #keeps track of the number of valid data sets so we know how to average
-PSXAve = [0]*1024
-for dataSet in xacel[startIndex:endIndex]:
-    [ps,freq] = CalcPowerSpec(dataSet,.02)                             #get the power spectrum
-    if len(ps) == 1024:                                                 #check to see if power spectrum is valid length
-        validSets += 1                                                      #if it is, incrememnt the count
-        PSXAve = [x + y for x,y in zip(PSXAve,ps)]                      #and add the new ps to the total sum
-
-PSXAve = [x/validSets for x in PSXAve]                                      #divide each element by the total amount to get the average
-
-plt.figure(6)
 plt.plot(freq,PSXAve)
 
 plt.figure(3)
