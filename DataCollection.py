ff620cd3f1a80e750f456fd3e2d42da8b9007fb9
Ryan <ryanzhan911@gmail.com>
2020-02-06 22:18:50 -0800

    added time stamp and changed file opening method

--- DataCollection.py
+++ DataCollection.py
@@ -4,8 +4,7 @@ import datetime
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('serPort', type = int, 
-    help='the serial adress 1 refers to port 1 2 to port 2')
+parser.add_argument('serPort', help='the serial adress 1 refers to port 1 2 to port 2')
 
 args = parser.parse_args()
 #Grab today's date and create a file using it.
@@ -14,24 +13,23 @@ filename = str(today).replace(" ","_")
 filename = filename.replace(':','_')
 filename = filename.replace('.','_')
 
-dataFile = open(filename+'.txt',"w")
-
-serialAddress = ""
-if(args.serPort == 1):
-    serialAddress = "/dev/ttyUSB1"
-elif(args.serPort ==2):
-    serialAddress = "/dev/ttyUSB2"
 
 #open serial connection.
-ser = serial.Serial("/dev/ttyUSB1") #Remember to put in the address.
+ser = serial.Serial(arg.SerPort) #Remember to put in the address.
 ser.open()
 ser.reset_input_buffer() #might also be:
 #ser.flushInput()
 
-while True:
-    if(ser.in_waiting() > 0):
-        data = ser.readLine()
-        dataFile.write(str(data))
-        print("Wrote a line!")
+dataFile = open()
+with open(filename+'.txt',"w") as textfile:
+#serialAddress = arg.serPort
+    while True:
+        if(ser.in_waiting() > 0):
+            data = ser.readLine()
+            dataFile.write(str(data))
+            #I'm not sure what the end of line char is so I'm just going to write a line after
+            if data[0]=='$':
+                dataFile.write(str(today)+'\n') #write time stamp
+            print("Wrote a line!")
 
 
